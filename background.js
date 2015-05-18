2ca891f6c6b5bc153914737bcc13c6dd3fea30d9
DaniGuardiola <contacto@daniguardiola.me>
2015-05-18 19:46:52 +0200

    Update

--- background.js
+++ background.js
@@ -3,3 +3,29 @@ chrome.browserAction.onClicked.addListener(function(tab) {
         file: "script.js"
     });
 });
+
+window.addEventListener("load", init);
+
+function init() {
+    checkVersion();
+}
+
+function checkVersion() {
+    var landingUrl = "http://www.carlosjeurissen.com/black-menu-for-google/landing/index.html";
+    var oldVersion = localStorage.getItem("versionNumber");
+    var version = chrome.app.getDetails().version;
+    var updatedUrl = "http://www.carlosjeurissen.com/black-menu-for-google/landing/index.html?update=" + version;
+
+    if (!oldVersion) {
+        openPopup(landingUrl);
+    } else if (oldVersion !== version) {
+        openPopup(updatedUrl);
+    }
+    localStorage.setItem("versionNumber", version);
+}
+
+function openPopup(url) {
+    chrome.tabs.create({
+        "url": url
+    });
+}
